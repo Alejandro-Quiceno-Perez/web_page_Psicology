fd8037179285a43b92b82144aefa16f9b1d0e322
Alejandro Quiceno Perez <alejo.perez2905@gmail.com>
2025-10-15 16:52:07 -0500

    Actualizacion de componentes, fuentes, imagenes

--- src/utils/LogoImage.jsx
+++ src/utils/LogoImage.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const LogoImage = () => {
     return (
         <div className="container-logo">
-            <img className="img-logo" src="./img/Logo_sinFondo.png" alt="Logo" />
+            <img className="img-logo" src="./img/img_header.png" alt="Logo" />
             <h1 className="title-logo">METANOIA</h1>
         </div>
     )
--- src/utils/WelcomeBanner.jsx
+++ src/utils/WelcomeBanner.jsx
@@ -5,7 +5,7 @@ const WelcomeBanner = () => {
     return (
         <div className='container-info'>
             <section className="container-title">
-                <h1>Te acompañamos en tu camino hacia el <span>Bienestar Emocional</span></h1>
+                <h1>Te acompañamos en tu camino hacia el <span>BIENESTAR EMOCIONAL</span></h1>
                 <p>Un espacio seguro y cálido donde encontrarás el apoyo profesional que necesitas. Juntos construiremos herramientas para tu crecimiento personal y emocional.
                 </p>
                 <div className="btn-info-container">
@@ -17,7 +17,7 @@ const WelcomeBanner = () => {
             </section>
             <section className="container-img">
                 <div className="container__img">
-                    <img src="./img/lugar.png" alt="Imagen de fondo lugar" />
+                    <img src="./img/Logo_sinFondo.png" alt="Imagen de fondo lugar" />
                 </div>
             </section>
         </div>
