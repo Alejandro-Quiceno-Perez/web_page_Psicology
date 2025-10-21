ff7e76e0894c8e9c65f4d71fdb756cb585d1820e
Alejandro-Quiceno-Perez <alejo.perez2905@gmail.com>
2025-10-21 11:24:40 -0500

    Actualizaciones

--- src/utils/AboutUs.jsx
+++ src/utils/AboutUs.jsx
@@ -1,25 +1,39 @@
 import React from 'react'
+import '../styles/aboutUs.css'
 
 const AboutUs = () => {
     return (
         <div className='container-aboutUs'>
-            <div className="about-us">
+            <div className="quienes_somos">
                 <h1>Quienes somos</h1>
                 <ul>
                     <li>
-                        <p>Somos un espacio de bienestar creado para acompañarte en tus procesos personales, de pareja, familiares y organizacionales.
-Un lugar donde la escucha, la empatía y la presencia consciente se encuentran con la ciencia y la experiencia profesional.</p>
+                        <p>Somos un espacio de bienestar creado para acompañarte en tus procesos personales, de pareja, familiares y organizacionales. Un lugar donde la escucha, la empatía y la presencia consciente se encuentran con la ciencia y la experiencia profesional.</p>
                     </li>
                     <li>
                         <p>Nuestro equipo está conformado por psicólogos clínicos, psicólogos organizacionales y trabajadores sociales que comparten un mismo propósito: cuidar la mente, las emociones y las relaciones desde un enfoque humano, ético y basado en evidencia</p>
                     </li>
                     <li>
-                        <p>Creemos que el bienestar no es solo la ausencia de dolor, sino la posibilidad de vivir con sentido, equilibrio y resiliencia.
-Por eso, integramos prácticas terapéuticas con herramientas de mindfulness y una mirada de vida consciente, reconociendo la diversidad y honrando la singularidad de cada historia</p>
+                        <p>Creemos que el bienestar no es solo la ausencia de dolor, sino la posibilidad de vivir con sentido, equilibrio y resiliencia. Por eso, integramos prácticas terapéuticas con herramientas de mindfulness y una mirada de vida consciente, reconociendo la diversidad y honrando la singularidad de cada historia</p>
                     </li>
                     <li>
                         <p>Nuestra labor está guiada por tres pilares esenciales: confidencialidad, integridad y calidad del cuidado</p>
                     </li>
+                </ul>
+            </div>
+            <div className="que_hacemos">
+                <h1>Qué hacemos</h1>
+                <ul>
+                    <li>
+                        <p>Brindar salud mental integral y desarrollo humano para individuos, familias y organizaciones, guiados por principios de compasion, conciencia y ética profesional.</p>
+                    </li>
+                    <li>
+                        <p>Promover la salud mental y el desarrollo humano desde una perspectiva integral y con un énfasis espiritual consciente.</p>
+                    </li>
+                    <li>
+                        <p>Nuestro equipo combina experiencia clínica, investigación basada en evidencia y prácticas de mindfulness para acompañar a personas, parejas, familias y equipos de trabajo hacia estados de bienestar más profundos y sostenibles.</p>
+                    </li>
+
                 </ul>
             </div>
         </div>
